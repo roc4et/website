07012c1f640a4ec4dd41e09f58af90acaee68dea
roc4et <git@stackblitz.com>
2024-11-25 14:08:27 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'roc4et | Digital Creator',
+  description: 'Welcome to the digital space of roc4et',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
